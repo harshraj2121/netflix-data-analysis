c07f6ad50203a33b14d7e1fd2f6d256d03030ad0
harshraj2121 <harsh.r2121@hotmail.com>
2026-06-03 01:00:05 +0530

    some changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #read csv
 df = pd.read_csv('./dataset/netflix_titles.csv')
@@ -37,5 +38,31 @@ df['duration'] = df['duration'].fillna(median_duration)
 
 
 
+# chart 1 (Movies vs Tv shows)
+# categories = df['type'].value_counts().index
+# values = df['type'].value_counts().values
 
+# plt.bar(categories, values)
+# plt.title('Distribution of Movies and TV Shows on Netflix')
+# plt.xlabel('Type')
+# plt.ylabel('Count')
 
+# chart 2 (Top 10 countries with most movies and tv shows)
+countries = (
+    df['country']
+    .str.split(',')
+    .explode()
+    .str.strip()
+    .value_counts()
+    .head(10)
+)
+plt.figure(figsize=(10, 6))
+plt.barh(countries.index, countries.values)
+plt.gca().invert_yaxis()  # Reverse the y-axis to have the highest count at the top
+plt.title('Top 10 Countries with Most Movies and TV Shows on Netflix')
+plt.xlabel('Count')
+plt.ylabel('Countries')
+plt.tight_layout()
+plt.show()
+
+# print(countries)
