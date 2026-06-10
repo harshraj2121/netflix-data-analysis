78bc62c2a73806f80ed6700d938bbcf9998880cb
harshraj2121 <harsh.r2121@hotmail.com>
2026-06-10 22:28:40 +0530

    message

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ df['duration'] = df['duration'].fillna(median_duration)
 # plt.ylabel('Countries')
 # plt.tight_layout()
 
-#chart 3 (content Release trend on Netflix)
+
+#chart 3 (content Release trend on Netflix release_year)
 # plt.figure(figsize=(12,6))
 # plt.plot(
 #     df['release_year'].value_counts().sort_index().index,
@@ -75,17 +76,114 @@ df['duration'] = df['duration'].fillna(median_duration)
 # plt.ylabel("Number of Titles")
 # plt.title("Netflix Content Released by Year")
 
+
+
+# chart 4 (Distribution of TV Shows and Movies by Genre on Netflix top genres)
+# listed_in = df['listed_in'].str.split(',').explode().str.strip().value_counts()
+# tv_shows = listed_in[listed_in.index.str.contains('TV')]
+# not_tv_shows = listed_in[~listed_in.index.str.contains('TV')]
+
+# plt.figure(figsize=(15, 10))
+# plt.barh(tv_shows.index, tv_shows.values, label='TV Shows', color='blue')
+# plt.barh(not_tv_shows.index, not_tv_shows.values, label='Movies', color='orange')
+# plt.gca().invert_yaxis()
+# plt.xlabel('Count')
+# plt.title('Distribution of TV Shows and Movies by Genre on Netflix')
+# plt.legend()
+# plt.tight_layout()
+
+
+
+# chart 5 (Distribution of content rating on Netflix)
+# ratings = df['rating'].value_counts().head(10)
+# plt.barh(ratings.index, ratings.values)
+# plt.gca().invert_yaxis()
+# plt.xlabel('count')
+# plt.ylabel('Rating')
+# plt.title('Distribution of Content rating on Netflix')
+# plt.tight_layout()
+
+
+
+#chart 6 (Diatrubition of duration on Netflix)
+# movies_duration = df[df['type'] == 'Movie']['duration'].dropna().str.extract(r'(\d+)')[0].dropna().astype(int).sort_values()
+# bins = np.arange(
+#     0,
+#     movies_duration.max() + 20,
+#     20
+# )
+# plt.figure(figsize=(10, 4))
+# counts, bins, patches = plt.hist(
+#     movies_duration,
+#     bins=bins,
+#     edgecolor='black'
+# )
+# plt.yscale('log')
+# plt.ylim(1,max(counts) * 6)
+# plt.xticks(bins)
+# plt.yscale('log')
+# plt.title("Movie Duration Distribution")
+# plt.xlabel("Duration (Minutes)")
+# plt.ylabel("Frequency")
+
+
+
+
+#chart 7 (Top Directors on Netflix)
+# directors = df['director'].str.split(',').explode().str.strip().value_counts().head(11)
+# top_directors = directors[directors.index != 'Unknown']
+# plt.figure(figsize=(10, 6))
+# plt.barh(top_directors.index, top_directors.values)
+# plt.gca().invert_yaxis()
+# plt.xlabel('No. of Movies')
+# plt.ylabel('Director')
+# plt.title('Top Directors on Netflix')
+
+
+
+
+
+# chart 8 (Top Actors on Netflex)
+# actors = df['cast'].str.split(',').explode().str.strip().value_counts().head(11)
+# top_actors = actors[actors.index != 'Unknown']
+# plt.figure(figsize=(10, 6))
+# plt.subplots_adjust(
+#     left=0.20,
+#     right=0.97,
+#     top=0.90,
+#     bottom=0.12
+# )
+# plt.barh(top_actors.index, top_actors.values)
+# plt.tight_layout()
+# plt.gca().invert_yaxis()
+# plt.xlabel('No. of Movies')
+# plt.ylabel('Actor')
+# plt.title('Top Actors on Netflix')
+
+
+
+
+#part 2
+# chart 1 (Movies vs TV Shows Over Time in past 10 years)
+# yearly_content = df.groupby(['release_year', 'type']).size().unstack(fill_value=0).tail(10)
+# yearly_content.plot(
+#     kind='bar',
+#     stacked=True,
+#     figsize=(14,6)
+# )
+
+
+# chart 2 ( top geners by type )
 listed_in = df['listed_in'].str.split(',').explode().str.strip().value_counts()
 tv_shows = listed_in[listed_in.index.str.contains('TV')]
 not_tv_shows = listed_in[~listed_in.index.str.contains('TV')]
-plt.figure(figsize=(10, 6))
-plt.barh(tv_shows.index, tv_shows.values, label='TV Shows', color='blue')
-plt.barh(not_tv_shows.index, not_tv_shows.values, label='Movies', color='orange')
-plt.gca().invert_yaxis()
-plt.xlabel('Count')
-plt.title('Distribution of TV Shows and Movies by Genre on Netflix')
-plt.legend()
-plt.tight_layout()
+# top_genres = df.groupby(['type', 'listed_in']).size().unstack(fill_value=0)
+# print(top_genres)
+
+
+
+
+
 
 
 
